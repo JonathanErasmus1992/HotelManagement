ed41bcf40e3989aecb28b6a474913b64c2d00f59
Jonathan <jonathanerasmus1992@gmail.com>
2015-09-13 20:33:14 +0200

    Domain commit

---
+++ src/main/java/hotelmanagement/services/BookingService.java
@@ -0,0 +1,21 @@
+package hotelmanagement.services;
+
+import hotelmanagement.domain.Booking;
+import hotelmanagement.domain.Room;
+import hotelmanagement.domain.ServicesAndAddOns;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by student on 2015/09/13.
+ */
+public interface BookingService {
+    public List<Booking> getAllBookings();
+    public boolean createBooking(String referenceNumber, List<Room>roomList, List<ServicesAndAddOns> servicesAndAddOnsList, Date hireDate);
+    public boolean deleteBooking(String referenceNumber);
+}
+/*
+    No updating just remove room from room list
+    Same with Add Ons
+ */
